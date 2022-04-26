70637e805dc45a13900f9dcee99f17cfc95091a0
Matt Elvidge <elvidgematthew@yahoo.com>
2022-04-26 15:29:08 -0400

    js additions
    minor javasript edits

--- js/script.js
+++ js/script.js
@@ -9,3 +9,19 @@ function setActive(x){
   var y = document.getElementById("tag1");
   y.className+= "active";
 }
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+function naviToggler(){
+  var x = document.getElementById("navi-toggle");
+  x.checked= !x.checked;
+
+}
